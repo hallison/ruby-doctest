851b77d5eb8a874bb5339bc37499d341cd128e2a
Hallison Batista <hallisonbatista@gmail.com>
2024-11-25 18:58:41 -0300

    Main module

---
+++ lib/doctest.rb
@@ -0,0 +1,13 @@
+module DocTest
+end
+
+require 'irb'
+require_relative 'doctest/configuration'
+require_relative 'doctest/runner'
+require_relative 'doctest/lines'
+require_relative 'doctest/statement'
+require_relative 'doctest/result'
+require_relative 'doctest/special_directive'
+require_relative 'doctest/code_block'
+require_relative 'doctest/test'
+
